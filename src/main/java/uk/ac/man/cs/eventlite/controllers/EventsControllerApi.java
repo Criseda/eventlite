000b81da59bac861652f65e1546ce6bbc10111fd
criseda <laurentiu.cristian.preda@gmail.com>
2025-02-19 18:45:30 +0000

    Add api endpoints for deleting events #11

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
@@ -10,8 +10,10 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,4 +51,21 @@ public class EventsControllerApi {
 		return eventAssembler.toCollectionModel(eventService.findAll())
 				.add(linkTo(methodOn(EventsControllerApi.class).getAllEvents()).withSelfRel());
 	}
+	
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> deleteEvent(@PathVariable("id") long id) {
+		if (!eventService.existsById(id)) {
+			throw new EventNotFoundException(id);
+		}
+		eventService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+    @DeleteMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteAllEvents() {
+        eventService.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
 }
