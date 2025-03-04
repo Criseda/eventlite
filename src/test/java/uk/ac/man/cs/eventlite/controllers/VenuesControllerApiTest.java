ad1d1396cb4d101f7775abb674ff592bb13d0347
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2025-03-04 17:32:06 +0000

    Add Venue test files #34

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiTest.java
@@ -0,0 +1,46 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import uk.ac.man.cs.eventlite.assemblers.VenueModelAssembler;
+import uk.ac.man.cs.eventlite.config.Security;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(VenuesControllerApi.class)
+@Import({ Security.class, VenueModelAssembler.class })
+public class VenuesControllerApiTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Mock
+    private Venue venue;
+
+    @MockBean
+    private VenueService venueService;
+    
+    @MockBean
+    private EventService eventService;
+}
