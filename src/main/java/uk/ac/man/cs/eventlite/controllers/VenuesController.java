3cf21a799ffbeaf39125ce10da5b127812f80dba
sam fahim <sam.fahim@student.manchester.ac.uk>
2025-03-04 13:57:06 +0000

    issue #33 added necessary venue files for week 6

---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -0,0 +1,41 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import jakarta.validation.Valid;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
+
+@Controller
+@RequestMapping(value = "/venues", produces = { MediaType.TEXT_HTML_VALUE })
+public class VenuesController {
+
+	@Autowired
+	private EventService eventService;
+
+	@Autowired
+	private VenueService venueService;
+	
+	
+};
---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
@@ -0,0 +1,59 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import uk.ac.man.cs.eventlite.assemblers.EventModelAssembler;
+import uk.ac.man.cs.eventlite.assemblers.VenueModelAssembler;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
+import uk.ac.man.cs.eventlite.exceptions.VenueNotFoundException;
+
+@RestController
+@RequestMapping(value = "/api/venues", produces = { MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE })
+public class VenuesControllerApi {
+	
+
+	@Autowired
+    private VenueService venueService; 
+	
+	@Autowired
+	private VenueModelAssembler venueAssembler;
+
+
+	public EntityModel<Event>getVenue(long id) {
+		throw new VenueNotFoundException(id);
+	}
+
+	
+	@GetMapping
+	public CollectionModel<EntityModel<Venue>> getAllVenues() {
+		return venueAssembler.toCollectionModel(venueService.findAll())
+				.add(linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel());
+	}
+	
+};
