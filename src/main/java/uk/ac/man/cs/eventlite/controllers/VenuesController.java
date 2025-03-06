5dc14f2bc9c472bd222816d72f05d0f199624e81
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2025-03-06 23:25:48 +0000

    Venues GET All implementation (some get one)

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -1,5 +1,7 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,18 +24,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import jakarta.validation.Valid;
-import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
-import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
 import uk.ac.man.cs.eventlite.exceptions.VenueNotFoundException;
 
 @Controller
 @RequestMapping(value = "/venues", produces = { MediaType.TEXT_HTML_VALUE })
 public class VenuesController {
-
-	@Autowired
-	private EventService eventService;
 
 	@Autowired
 	private VenueService venueService;
@@ -45,4 +42,27 @@ public class VenuesController {
 
 		return "venues/not_found";
 	}
+	
+	@GetMapping("/{id}")
+	public String getVenue(@PathVariable("id") long id, Model model) {
+		if (!venueService.existsById(id)) {
+			throw new VenueNotFoundException(id);
+		}
+		model.addAttribute("e", venueService.findById(id).get());
+		return "venues/details";
+	}
+	
+    @GetMapping
+    public String getAllVenues(@RequestParam(value = "search", required = false) String search, Model model) {
+        if (search != null && !search.isEmpty()) {
+            // If search parameter is provided, filter venues by name
+            model.addAttribute("venues", venueService.findByNameContainingIgnoreCase(search));
+            model.addAttribute("search", search);
+        } else {
+            // Otherwise, get all venues
+            model.addAttribute("venues", venueService.findAll());
+        }
+        
+        return "venues/index";
+    }
 };
--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
@@ -21,48 +21,37 @@ public class VenueServiceImpl implements VenueService {
 
 	private final static Logger log = LoggerFactory.getLogger(VenueServiceImpl.class);
 
-	private final static String DATA = "data/venues.json";
-
 	@Autowired
 	private VenueRepository venueRepository;
 	
 	@Override
 	public long count() {
-//		long count = 0;
-//		Iterator<Venue> i = findAll().iterator();
-//
-//		for (; i.hasNext(); count++) {
-//			i.next();
-//		}
-//
-//		return count;
 		return venueRepository.count();
 	}
 
 	@Override
 	public Iterable<Venue> findAll() {
-//		Iterable<Venue> venues;
-//
-//		try {
-//			ObjectMapper mapper = new ObjectMapper();
-//			InputStream in = new ClassPathResource(DATA).getInputStream();
-//
-//			venues = mapper.readValue(in, mapper.getTypeFactory().constructCollectionType(List.class, Venue.class));
-//		} catch (Exception e) {
-//			// If we can't read the file, then the event list is empty...
-//			log.error("Exception while reading file '" + DATA + "': " + e);
-//			venues = Collections.emptyList();
-//		}
-//
-//		return venues;
 		return venueRepository.findAll();
 	}
 	
+	@Override
 	public Venue save(Venue venue) {
 		return venueRepository.save(venue);
 	}
 	
+	@Override
 	public Optional<Venue> findById(long id) {
 		return venueRepository.findById(id);
+	}
+	
+	@Override
+	public boolean existsById(long id) {
+		return venueRepository.existsById(id);
+	}
+	
+	@Override
+	public Object findByNameContainingIgnoreCase(String search) {
+		// TODO: SEARCH TEAM TO IMPLEMENT
+		return null;
 	}
 }
