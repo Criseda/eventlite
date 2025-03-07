a8f574dd56715b07498368451e50c18b191374b7
a12778dp <david.pupla@student.manchester.ac.uk>
2025-03-07 15:23:22 +0000

    Added tests for creating new venues functionality.

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -1,5 +1,6 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Collections;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +35,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import uk.ac.man.cs.eventlite.config.Security;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
@@ -51,4 +54,119 @@ public class VenuesControllerTest {
 
     @MockBean
     private EventService eventService;
+    
+    @Test
+    public void CreateVenueSuccess() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "2400")
+        .param("street", "Oxford Road") 
+		.param("postcode", "M14 6HX"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/venues"))
+        .andExpect(flash().attributeExists("ok_message"));
+
+    	verify(venueService).save(any(Venue.class));
+    }
+    
+    @Test
+    public void EmptyName() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "")
+        .param("capacity", "2400")
+        .param("street", "Oxford Road") 
+		.param("postcode", "M14 6HX"))
+        .andExpect(model().attributeHasFieldErrors("venue", "name"))
+        .andExpect(view().name("venues/new"));
+    }
+    
+    @Test
+    public void BadPostcode() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "2400")
+        .param("street", "Oxford Road") 
+		.param("postcode", "abc 123"))
+        .andExpect(model().attributeHasFieldErrors("venue", "postcode"))
+        .andExpect(view().name("venues/new"));
+    }
+    
+    @Test
+    public void EmptyPostcode() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "2400")
+        .param("street", "Oxford Road") 
+		.param("postcode", ""))
+        .andExpect(model().attributeHasFieldErrors("venue", "postcode"))
+        .andExpect(view().name("venues/new"));
+    }
+    
+    @Test
+    public void NoCapacity() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "0")
+        .param("street", "Oxford Road") 
+		.param("postcode", "M14 6HX"))
+        .andExpect(model().attributeHasFieldErrors("venue", "capacity"))
+        .andExpect(view().name("venues/new"));
+    }
+    
+    @Test
+    public void EmptyCapacity() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "")
+        .param("street", "Oxford Road") 
+		.param("postcode", "M14 6HX"))
+        .andExpect(model().attributeHasFieldErrors("venue", "capacity"))
+        .andExpect(view().name("venues/new"));
+    }
+    
+    @Test
+    public void BlankSpaceStreet() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "2400")
+        .param("street", "   ") 
+		.param("postcode", "M14 6HX"))
+        .andExpect(model().attributeHasFieldErrors("venue", "street"))
+        .andExpect(view().name("venues/new"));
+    }
+    
+    @Test
+    public void EmptyStreet() throws Exception{
+    	mvc.perform(post("/venues/save")
+    	.with(user("Rob").roles(Security.ADMIN))
+        .with(csrf())
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("name", "Oak House")
+        .param("capacity", "2400")
+        .param("street", "") 
+		.param("postcode", "M14 6HX"))
+        .andExpect(model().attributeHasFieldErrors("venue", "street"))
+        .andExpect(view().name("venues/new"));
+    }
 }
