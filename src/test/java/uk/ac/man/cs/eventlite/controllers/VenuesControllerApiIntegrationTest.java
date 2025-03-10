cc97816701dec6501738b207489fb2674f16571f
Adam <adammarvin05@gmail.com>
2025-03-11 01:01:27 +0400

    Created tests for deleting venue which all pass.

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiIntegrationTest.java
@@ -1,8 +1,12 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,9 +21,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.config.Security;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -170,4 +182,59 @@ public class VenuesControllerApiIntegrationTest extends AbstractTransactionalJUn
 				.contentType(MediaType.APPLICATION_JSON).expectBody().jsonPath("$._embedded.venues.length()").isEqualTo(currentRows);
 	}
 
+	@Test
+	public void deleteVenueAsForbiddenRole() {
+	    int currentRows = countRowsInTable("venues");
+
+	    // Simulate an ATTENDEE attempting to delete an event
+	    client.mutate().filter(basicAuthentication("Tom", "Carroll")) // Tom is an ATTENDEE
+	                   .build()
+	                   .delete()
+	                   .uri("/venues/1")
+	                   .accept(MediaType.APPLICATION_JSON)
+	                   .exchange()
+	                   .expectStatus()
+	                   .isForbidden(); // Expect a 403 Forbidden status
+
+	    // Check that nothing is removed from the database
+	    assertThat(currentRows, equalTo(countRowsInTable("venues")));
+	}
+	
+	
+	@Test
+	@DirtiesContext
+	public void deleteEventWithUser() {
+		int currentRows = countRowsInTable("venues");
+		
+		client.mutate().filter(basicAuthentication("Rob", "Haines"))
+		               .build()
+		               .delete()
+		               .uri("/venues/1")
+		               .accept(MediaType.APPLICATION_JSON)
+		               .exchange()
+		               .expectStatus()
+		               .isNoContent()
+		               .expectBody()
+		               .isEmpty();
+
+		
+		//Check one row got removed from the database
+		assertThat(currentRows - 1, equalTo(countRowsInTable("venues")));
+	}
+	@Test
+	public void deleteEventNotFound() {
+		int currentRows = countRowsInTable("events");
+		
+		client.mutate().filter(basicAuthentication("Rob", "Haines"))
+		               .build()
+		               .delete()
+		               .uri("/venues/99")
+		               .accept(MediaType.APPLICATION_JSON)
+		               .exchange().expectStatus()
+		               .isNotFound();
+
+		
+		//Check nothing is removed from the database
+		assertThat(currentRows, equalTo(countRowsInTable("venues")));
+	}
 }
