c279b857cfa139cfee93ba554ed436d570ddc6e0
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2025-03-25 14:20:59 +0000

    Refactor MastodonService and integrate timeline retrieval in EventsController

--- src/main/java/uk/ac/man/cs/eventlite/services/MastodonService.java
+++ src/main/java/uk/ac/man/cs/eventlite/services/MastodonService.java
@@ -1,16 +1,12 @@
 package uk.ac.man.cs.eventlite.services;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
 import com.sys1yagi.mastodon4j.MastodonClient;
-import com.sys1yagi.mastodon4j.MastodonRequest;
-import com.sys1yagi.mastodon4j.api.Range;
 import com.sys1yagi.mastodon4j.api.entity.Status;
 import com.sys1yagi.mastodon4j.api.entity.Status.Visibility;
 import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException;
@@ -22,36 +18,37 @@ import okhttp3.OkHttpClient;
 
 @Service
 public class MastodonService {
-	
+
 	private String accessToken = Dotenv.load().get("MASTODON_ACCESS_TOKEN");
-	
+
 	private String serverURL = "techhub.social";
 
-	private  MastodonClient createClient() {
+	private MastodonClient createClient() {
 		return new MastodonClient.Builder(serverURL, new OkHttpClient.Builder(), new Gson())
-             .accessToken(accessToken)
-             .useStreamingApi()
-             .build();
+				.accessToken(accessToken)
+				.useStreamingApi()
+				.build();
 	}
-	
+
 	public Status postStatus(String content) throws Mastodon4jRequestException {
 		MastodonClient client = createClient();
 		Statuses statuses = new Statuses(client);
-		
+
 		return statuses.postStatus(content, null, null, false, null, Visibility.Unlisted).execute();
 	}
-	
+
 	public List<Status> getTimeline() {
 		try {
-		MastodonClient client = createClient();
-		Timelines timeline = new Timelines(client);
-		
-		return timeline.getHome().execute().getPart();
-		
+			MastodonClient client = createClient();
+			Timelines timeline = new Timelines(client);
+
+			// Get home timeline
+			return timeline.getHome().execute().getPart();
 		} catch (Mastodon4jRequestException e) {
+			e.printStackTrace();
 			return Collections.emptyList();
 		}
-		
+
 	}
-	
+
 }
